0b2f847761af39448123c2d35e28b605fce148da
shimizu <nichibishimizu@gmail.com>
2024-10-22 15:59:28 +0900

    5-3-3まで完了

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -1,3 +1,16 @@
+import { createClient } from "microcms-js-sdk";
+import type {
+    MicroCMSQueries,
+    MicroCMSImage,
+    MicroCMSListContent,
+} from "microcms-js-sdk";
+
+export type Member = {
+    name: string;
+    position: string;
+    profile: string;
+    image: MicroCMSImage;
+} & MicroCMSListContent;
 
 export type Category = {
     name: string;
@@ -11,4 +24,25 @@ export type News = {
     };
     publishedAt: string;
     createdAt: string;
+};
+
+if (!process.env.MICROCMS_SERVICE_DOMEIN) {
+    throw new Error("MICROCMS_SERVICE_DOMEIN is required");
+}
+if (!process.env.MICROCMS_API_KEY) {
+    throw new Error("MICROCMS_API_KEY is required");
+}
+
+const client = createClient({
+    serviceDomain: process.env.MICROCMS_SERVICE_DOMEIN,
+    apiKey: process.env.MICROCMS_API_KEY,
+});
+
+export const getMembersList = async (queries?: MicroCMSQueries) => {
+    const listData = await client
+    .getList<Member>({
+        endpoint: "members",
+        queries,
+    });
+    return listData;
 };
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { hostname } from 'os';
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "images.microcms-assets.io",
+            },
+        ],
+    },
+};
 export default nextConfig;
