ad1bafe567504ddd63aaf044cdfb6c805f9136b2
shimizu <nichibishimizu@gmail.com>
2024-10-28 08:59:10 +0900

    5章完了

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import styles from "./page.module.css";
 import Image from "next/image";
 import { getNewsList } from "@/app/_libs/microcms";
-import { TOP_NEWS_LIMIT } from "@/app/_libs/microcms";
+import { TOP_NEWS_LIMIT } from "@/app/_constants";
 import NewsList from "@/app/_components/NewsList";
 import ButtonLink from "@/app/_components/ButtonLink";
 
 import { url } from "inspector";
 import { profile } from "console";
 
-export default function Home() {
+export default async function Home() {
   const data = await getNewsList({
     limit: TOP_NEWS_LIMIT,
   });
