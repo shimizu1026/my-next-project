99af149e81ca781a293fff7320e91201def8a890
shimizu <nichibishimizu@gmail.com>
2024-11-13 09:05:57 +0900

    7章まで完了

--- app/_components/SearchField/index.tsx
+++ app/_components/SearchField/index.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import styles from "./index.module.css";
+import { Suspense } from "react";
 
-export default function SearchField() {
+function SearchFieldComponent() {
     const router = useRouter();
+    const searchParams = useSearchParams();
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -20,8 +22,16 @@ export default function SearchField() {
         <form onSubmit={handleSubmit} className={styles.form}>
             <label className={styles.search}>
                 <Image src="/search.svg" alt="検索" width={16} height={16} loading="eager"/>
-                <input type="text" name="q" placeholder="キーワードを入力" className={styles.searchInput} />
+                <input type="text" name="q" defaultValue={searchParams.get("q") ?? undefined} placeholder="キーワードを入力" className={styles.searchInput} />
             </label>
         </form>
+    );
+}
+
+export default function SearchField() {
+    return (
+        <Suspense>
+            <SearchFieldComponent/>
+        </Suspense>
     );
 }
