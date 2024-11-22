0fe8d59250e0397ffd2563c56b31bfbee43a588e
shimizu <nichibishimizu@gmail.com>
2024-11-22 16:24:25 +0900

    Update page.tsx

--- app/news/category/[id]/p/[current]/page.tsx
+++ app/news/category/[id]/p/[current]/page.tsx
@@ -21,8 +21,8 @@ export default async function Page({params}: Props) {
 
   const { contents: news, totalCount } = await getNewsList({
     filters: `category[equals]${category.id}`,
-    limit: 10,
-    offset: 10 * (current - 1),
+    limit: NEWS_LIST_LIMIT,
+    offset: NEWS_LIST_LIMIT * (current - 1),
   });
 
   if (news.length === 0) {
