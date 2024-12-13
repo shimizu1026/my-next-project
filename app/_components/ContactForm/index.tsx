cafaa347d8435f33f4b34d878266249a79a82bc1
shimizu <nichibishimizu@gmail.com>
2024-12-13 10:28:28 +0900

    252

--- app/_components/ContactForm/index.tsx
+++ app/_components/ContactForm/index.tsx
@@ -1,8 +1,29 @@
+"use client";
+
+import { createContactData } from "@/app/_actions/contact";
+import { useFormState } from "react-dom";
 import styles from "./index.module.css";
 
+const initialState = {
+	status: "",
+	message: "",
+};
+
 export default function ContactForm() {
+	const [state, formAction] = 
+	useFormState(createContactData, initialState);
+	console.log(state);
+	if (state.status === "success") {
+		return (
+			<p className={styles.success}>
+				お問い合わせいただきありがとうございます。
+				<br />
+				お返事まで今しばらくお待ちください。
+			</p>
+		);
+	}
 	return (
-		<form className={styles.form}>
+		<form className={styles.form} action={formAction}>
 			<div className={styles.horizontal}>
 				<div className={styles.item}>
 					<label className={styles.label} htmlFor="lastname">
@@ -35,6 +56,9 @@ export default function ContactForm() {
 					<input type="text" className={styles.textfield} id="message" name="message" />
 				</div>
 				<div className={styles.actions}>
+					{state.status === "error" && (
+						<p className={styles.error}>{state.message}</p>
+					)}
 					<input type="text" value="送信する" className={styles.button} />
 				</div>
 			</div>
