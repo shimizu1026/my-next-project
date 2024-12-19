291fe882da5ea3cd0b4e9f691e60be746336f744
shimizu <nichibishimizu@gmail.com>
2024-12-19 13:08:37 +0900

    265

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,23 @@ import "./globals.css";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
 
+export const metadata: Metadata = {
+	metadataBase: new URL("http://localhost:3000"),
+	title: {
+		template: "%s | シンプルなコーポレートサイト",
+		default: "シンプルなコーポレートサイト",
+	},
+	description:"「Next.js＋ヘッドレスCMSではじめる！かんたん・モダンWebサイト制作入門」作成されるサイトです。",
+	openGraph: {
+		title: "シンプルなコーポレートサイト",
+		description: "「Next.js＋ヘッドレスCMSではじめる！かんたん・モダンWebサイト制作入門」作成されるサイトです。",
+		images: ["/ogp.png"],
+	},
+	alternates: {
+		canonical: "http://localhost:3000",
+	},
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
